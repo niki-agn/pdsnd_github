a060d8687ea347eb0b665638ddd9cf6a7170510d
Nikolett Adam-Gema <gema.nikolett@gmail.com>
2023-07-05 14:37:48 +0200

    docs: improve bikeshare docstrings

--- bikeshare.py
+++ bikeshare.py
@@ -202,7 +202,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users. Type, gender, birth year."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
@@ -234,7 +234,7 @@ def user_stats(df):
 
 
 def print_raw_data(df):
-    """Printing raw data"""
+    """Printing raw data of bikeshare.csvs"""
     answer = input("Do you want to see 5 lines of raw data? Enter yes or no.")
     if answer.lower() != 'yes':
         return
